a88360be1ff7a0a06c1cb5df8e62d56a1e8e850e
nicolassouzasantos <nicklite1@hotmail.com>
2024-10-20 16:26:10 -0300

    criado o texto da teoria

--- src/app/paginas/teoria/page.tsx
+++ src/app/paginas/teoria/page.tsx
@@ -1,6 +1,20 @@
 export default function Teoria(){
     return(
-        <div>é sobre essa teoria que tem que ser essa pagina The rotation of the Earth can be affected by electromagnetic fields.</div>
-        
+        <div>
+            <h1>Teoria: A rotação da terra pode ser afetada por campos eletromagnéticos</h1>
+            <p>
+                A teoria de Velikovsky propõe que um distúrbio no campo eletomagnético causado Vênus e Marte se aproximando da terra pode afetar a rotação da terra, o eixo de inclinação e o campo magnético <br />
+            </p>
+            <h1>Consequências: Catástrofes</h1>
+            <p>
+                Velikovsky sugere que terímo inúmeros problemas como: <br />
+
+                -Interrupção  da rotação da terra <br />
+
+                -Nivelamento das montanhas  <br />
+
+                -Enxames de moscas  <br />
+            </p>
+        </div>
     )
 }
